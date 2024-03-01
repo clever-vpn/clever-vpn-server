17942c856412a919bde73f364228eaeb40855ecc
wireguard-vpn <wubolin001@outlook.com>
2024-03-01 15:55:19 +0800

    2024030101

--- install.sh
+++ install.sh
@@ -30,40 +30,71 @@ function checkVirt() {
 }
 
 function checkOS() {
-  source /etc/os-release
-  OS="${ID}"
-  if [[ ${OS} == "debian" || ${OS} == "raspbian" ]]; then
-    if [[ ${VERSION_ID} -lt 10 ]]; then
-      echo "Your version of Debian (${VERSION_ID}) is not supported. Please use Debian 10 Buster or later"
-      exit 1
-    fi
-    OS=debian # overwrite if raspbian
-  elif [[ ${OS} == "ubuntu" ]]; then
-    RELEASE_YEAR=$(echo "${VERSION_ID}" | cut -d'.' -f1)
-    if [[ ${RELEASE_YEAR} -lt 18 ]]; then
-      echo "Your version of Ubuntu (${VERSION_ID}) is not supported. Please use Ubuntu 18.04 or later"
-      exit 1
-    fi
-  elif [[ ${OS} == "fedora" ]]; then
-    if [[ ${VERSION_ID} -lt 32 ]]; then
-      echo "Your version of Fedora (${VERSION_ID}) is not supported. Please use Fedora 32 or later"
-      exit 1
-    fi
-  elif [[ ${OS} == 'centos' ]] || [[ ${OS} == 'almalinux' ]] || [[ ${OS} == 'rocky' ]]; then
-    if [[ ${VERSION_ID} == 7* ]]; then
-      echo "Your version of CentOS (${VERSION_ID}) is not supported. Please use CentOS 8 or later"
-      exit 1
-    fi
-  elif [[ -e /etc/oracle-release ]]; then
-    source /etc/os-release
-    OS=oracle
-  elif [[ -e /etc/arch-release ]]; then
-    OS=arch
-  else
-    echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, AlmaLinux, Oracle or Arch Linux system"
+  ## kernel >= 5.6
+  required_major=5
+  required_minor=6
+  read -r current_major current_minor _ <<<"$(uname -r | tr '.' ' ')"
+
+  if ((current_major < required_major)) || ((current_major == required_major && current_minor < required_minor)); then
+    echo "Current Kernel version is $(uname -r). We need kernel version >= $required_major.$required_minor ! "
     exit 1
   fi
+
+  ## support systemd
+  if ! pgrep systemd; then
+    echo "Current Linux don't support systemd. We only support linux version with systemd!"
+    exit 1
+  fi
+   
+  ## support kernel modules 
+  if [[ ! -d "/lib/modules/$(uname -r)" ]]; then
+    echo "Don't include kernel modles!"
+    exit 1
+  fi
+
+  ## include make enviroment
+  if ! command -v "make" >/dev/null 2>&1; then
+    echo "Dont include make toolchain"
+    exit 1
+  fi
+
 }
+
+# function checkOS() {
+#   source /etc/os-release
+#   OS="${ID}"
+#   if [[ ${OS} == "debian" || ${OS} == "raspbian" ]]; then
+#     if [[ ${VERSION_ID} -lt 10 ]]; then
+#       echo "Your version of Debian (${VERSION_ID}) is not supported. Please use Debian 10 Buster or later"
+#       exit 1
+#     fi
+#     OS=debian # overwrite if raspbian
+#   elif [[ ${OS} == "ubuntu" ]]; then
+#     RELEASE_YEAR=$(echo "${VERSION_ID}" | cut -d'.' -f1)
+#     if [[ ${RELEASE_YEAR} -lt 18 ]]; then
+#       echo "Your version of Ubuntu (${VERSION_ID}) is not supported. Please use Ubuntu 18.04 or later"
+#       exit 1
+#     fi
+#   elif [[ ${OS} == "fedora" ]]; then
+#     if [[ ${VERSION_ID} -lt 32 ]]; then
+#       echo "Your version of Fedora (${VERSION_ID}) is not supported. Please use Fedora 32 or later"
+#       exit 1
+#     fi
+#   elif [[ ${OS} == 'centos' ]] || [[ ${OS} == 'almalinux' ]] || [[ ${OS} == 'rocky' ]]; then
+#     if [[ ${VERSION_ID} == 7* ]]; then
+#       echo "Your version of CentOS (${VERSION_ID}) is not supported. Please use CentOS 8 or later"
+#       exit 1
+#     fi
+#   elif [[ -e /etc/oracle-release ]]; then
+#     source /etc/os-release
+#     OS=oracle
+#   elif [[ -e /etc/arch-release ]]; then
+#     OS=arch
+#   else
+#     echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, AlmaLinux, Oracle or Arch Linux system"
+#     exit 1
+#   fi
+# }
 
 function initialCheck() {
   isRoot
