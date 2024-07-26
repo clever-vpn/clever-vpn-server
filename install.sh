a89231f7386a79ff1db33271e14b4dbf8df16ffb
wireguard-vpn <wubolin001@outlook.com>
2024-07-26 14:59:19 +0800

    2024072603

--- install.sh
+++ install.sh
@@ -141,41 +141,6 @@ function checkOS() {
 
 }
 
-# function checkOS() {
-#   source /etc/os-release
-#   OS="${ID}"
-#   if [[ ${OS} == "debian" || ${OS} == "raspbian" ]]; then
-#     if [[ ${VERSION_ID} -lt 10 ]]; then
-#       echo "Your version of Debian (${VERSION_ID}) is not supported. Please use Debian 10 Buster or later"
-#       exit 1
-#     fi
-#     OS=debian # overwrite if raspbian
-#   elif [[ ${OS} == "ubuntu" ]]; then
-#     RELEASE_YEAR=$(echo "${VERSION_ID}" | cut -d'.' -f1)
-#     if [[ ${RELEASE_YEAR} -lt 18 ]]; then
-#       echo "Your version of Ubuntu (${VERSION_ID}) is not supported. Please use Ubuntu 18.04 or later"
-#       exit 1
-#     fi
-#   elif [[ ${OS} == "fedora" ]]; then
-#     if [[ ${VERSION_ID} -lt 32 ]]; then
-#       echo "Your version of Fedora (${VERSION_ID}) is not supported. Please use Fedora 32 or later"
-#       exit 1
-#     fi
-#   elif [[ ${OS} == 'centos' ]] || [[ ${OS} == 'almalinux' ]] || [[ ${OS} == 'rocky' ]]; then
-#     if [[ ${VERSION_ID} == 7* ]]; then
-#       echo "Your version of CentOS (${VERSION_ID}) is not supported. Please use CentOS 8 or later"
-#       exit 1
-#     fi
-#   elif [[ -e /etc/oracle-release ]]; then
-#     source /etc/os-release
-#     OS=oracle
-#   elif [[ -e /etc/arch-release ]]; then
-#     OS=arch
-#   else
-#     echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, AlmaLinux, Oracle or Arch Linux system"
-#     exit 1
-#   fi
-# }
 
 function initialCheck() {
   if isRoot && checkVirt && checkOS; then
